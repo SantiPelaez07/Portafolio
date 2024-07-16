f20fd48bdc92a530252a168f2a0b7a3050fdf31f
SantiPelaez07 <102003216+SantiPelaez07@users.noreply.github.com>
2024-07-16 08:28:04 -0500

    Avances en los proyectos y correcciones generales

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         const id = entry.target.getAttribute("id");
         const menuLink = document.querySelector(`.mainBar a[href="#${id}"]`);
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             document.querySelector(".mainBar a.select").classList.remove("select")
             menuLink.classList.add("select")
         }
@@ -18,9 +18,34 @@ const observer = new IntersectionObserver((entries) => {
 menuLinks.forEach(menuLink => {
     const hash = menuLink.getAttribute("href");
     const target = document.querySelector(hash);
-    if(target){
+    if (target) {
         observer.observe(target)
     }
 });
 
+//  Envío de emails con la librería EmailJS
+
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        btn.value = 'Sending...';
+
+        const serviceID = 'default_service';
+        const templateID = 'template_cd9wzcu';
+
+        const message = document.querySelector("#message").value;
+        const templateParams = {
+            message: message
+        };
+
+        emailjs.sendForm(serviceID, templateID, templateParams)
+            .then(() => {
+                btn.value = 'Send Email';
+                alert('Sent!');
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
+            });
+    });
 
